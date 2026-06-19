ead45b64cf0fbc0e662236b51c368710c0d86115
Prem Kumar <premwest20@gmail.com>
2026-06-20 01:33:20 +0530

    Added Parabank jenkins framework

---
+++ src/test/java/fullPracticeJune/TestClassParaBank.java
@@ -0,0 +1,22 @@
+package fullPracticeJune;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class TestClassParaBank extends BaseTest{
+	
+	@Test
+	public void urlLaunch() {
+	
+		driver.get("https://parabank.parasoft.com/parabank/index.htm");
+	
+	
+	LoginPageParaBank loginpage = new LoginPageParaBank(driver, wait);
+	loginpage.loginPara("user12", "pass12");
+	System.out.println("Friday Jenkins practice");
+	
+	
+	}
+
+}
